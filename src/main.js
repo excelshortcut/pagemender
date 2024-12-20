91660de083bc464b63810dc972e43c9677e01c03
lcurtis <laurentzcurtis@gmail.com>
2024-12-20 15:53:51 -0600

    Fix duplicate const app in main.js

--- src/main.js
+++ src/main.js
@@ -3,14 +3,9 @@ import './index.css'  // This will be our main CSS file
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
-
 import Analytics from './plugins/analytics'
 
 const app = createApp(App)
 app.use(router)
-app.use(Analytics)  // Add this line to use the Analytics plugin
+app.use(Analytics)  // Add the Analytics plugin
 app.mount('#app')
