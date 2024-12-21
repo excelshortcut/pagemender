51e81ef4b3579234f5ccecc082711e52322b340b
lcurtis <laurentzcurtis@gmail.com>
2024-12-21 06:47:17 -0600

    Add Mailchimp tag

--- src/App.vue
+++ src/App.vue
@@ -106,6 +106,7 @@
                 class="flex-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 required
               />
+              <input type="hidden" name="tags" value="pagemenderps" />
               <input
                 type="submit"
                 value="Get Access"
